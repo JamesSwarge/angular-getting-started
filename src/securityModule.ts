9c75b306700349b4d00bc86ef43be7b24222836f
MyERP <tu.tran@yahoo.com>
2017-02-26 13:36:40 +0700

    refactor

--- src/securityModule.ts
+++ src/securityModule.ts
@@ -11,9 +11,10 @@ import { EditUser } from "./editUser";
 import {UserService} from "./userService";
 import {UserSummary} from "./userSummary";
 import {RedColor} from "./redColor";
+import {FormInput}  from "./formInput";
 @NgModule({
     imports: [BrowserModule, FormsModule, RouterModule, RouteConfig],
-    declarations: [DefaultLayout, DefaultPage, Users, AddNewUser, EditUser, UserSummary, RedColor],
+    declarations: [DefaultLayout, DefaultPage, Users, AddNewUser, EditUser, UserSummary, RedColor, FormInput],
     bootstrap: [DefaultLayout],
     providers:[]
 })
--- src/userSummary.ts
+++ src/userSummary.ts
@@ -1,17 +1,13 @@
-import { Output, EventEmitter, ElementRef, Component, Input } from "@angular/core";
+import { Output, EventEmitter, ElementRef, Component, Input, OnChanges } from "@angular/core";
 @Component({
     selector: "user-summary",
     templateUrl: "src/UserSummary.html"
 })
-export class UserSummary {
+export class UserSummary{
     @Input() firstName: string = null;
-    @Output() onFirstNameChanged: any = new EventEmitter<string>();
-
-    public printFirstName() {
-        console.log(this.firstName);
-    }
-    public onValueChange() {
-        console.log(this.firstName);
-        this.onFirstNameChanged.emit(this.firstName)
+    @Output() firstNameChange: any = new EventEmitter<string>();
+    public onChange(newValue: string) {
+        console.log(newValue);
+        this.firstNameChange.emit(newValue);
     }
 }
