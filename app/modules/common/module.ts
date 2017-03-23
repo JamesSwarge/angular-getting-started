194cdd3ede5fe024a15be4efeef60e7cdbc31d43
ORIENTSOFTWARE\tttu <tu.tran@orientsoftware.net>
2017-03-23 16:28:55 +0700

    update

--- app/modules/common/module.ts
+++ app/modules/common/module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import routeHelper from "./helpers/routeHelper";
 import {Script} from "./directives/script";
 import {Style} from "./directives/style";
+
+export default routeHelper;
+export * from "./models/appConfiguration";
+
 @NgModule({
     declarations:[Script, Style],
     exports: [Script, Style]
 })
 export class CommonModule { }
-
-export default routeHelper;
-export * from "./models/appConfiguration";
